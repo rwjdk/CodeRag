a4c0b26eb7b7c42e96a19c12d5d4759a1103b05c
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-04-25 11:30:05 +0200

    WIP

--- CodeRag/Workbench/Pages/XmlSummariesGeneration/XmlSummariesGenerationPage.razor.cs
+++ CodeRag/Workbench/Pages/XmlSummariesGeneration/XmlSummariesGenerationPage.razor.cs
@@ -1,74 +1,234 @@
-﻿using CodeRag.Shared.Ai;
+﻿using Blazor.Shared.Components;
+using Blazor.Shared;
+using CodeRag.Shared.Ai;
+using CodeRag.Shared.Chunking.CSharp;
 using CodeRag.Shared.EntityFramework.DbModels;
 using CodeRag.Shared.VectorStore;
 using Microsoft.AspNetCore.Components;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using MudBlazor;
 
 namespace Workbench.Pages.XmlSummariesGeneration;
 
-public partial class XmlSummariesGenerationPage(VectorStoreQuery vectorStoreQuery, AiQuery aiQuery)
+public partial class XmlSummariesGenerationPage(VectorStoreQuery vectorStoreQuery, CSharpChunker cSharpChunker, AiQuery aiQuery)
 {
-    private List<Data>? _data;
-    private Dictionary<string, bool>? _onlyUndocumentedCheckStates;
-    private VectorEntity? _selectEntry;
-    private string? _xmlSummary;
+    private ProjectSourceEntity[]? _cSharpSources;
+    private ProjectSourceEntity? _selectedSource;
+
+    [CascadingParameter]
+    public required BlazorUtils BlazorUtils { get; set; }
 
     [CascadingParameter]
     public required ProjectEntity Project { get; set; }
 
+    private TreeItemData<Item>? _tree;
+    private MudTreeView<Item>? _treeView;
+
+    private string? _searchPhrase;
+    private SummaryStatus _summaryStatus = SummaryStatus.All;
+    private CSharpKind _kind = CSharpKind.Method;
+    private VectorEntity[]? _existingVectorEntities;
+    private Item? _selectedItem;
+    private RProgressBar? _progressBar;
+
+    private async Task OnSearchTextChanged(string searchPhrase)
+    {
+        _searchPhrase = searchPhrase;
+        if (_treeView != null)
+        {
+            await _treeView.FilterAsync();
+        }
+    }
+
+    private Task<bool> MatchesName(TreeItemData<Item> item)
+    {
+        if (string.IsNullOrEmpty(item.Value!.Info.Name))
+        {
+            return Task.FromResult(false);
+        }
+
+        return Task.FromResult(_searchPhrase != null && item.Value.Info.Name.Contains(_searchPhrase, StringComparison.OrdinalIgnoreCase));
+    }
+
     protected override async Task OnInitializedAsync()
     {
-        VectorEntity[] sourceCode = await vectorStoreQuery.GetExistingAsync(Project.Id); //todo - on get code
+        //todo - this page throw exception is source location is GitHub
 
-        string[] kinds = sourceCode.Where(x => !string.IsNullOrWhiteSpace(x.Kind)).Select(x => x.Kind!).Distinct().ToArray();
-
-        List<Data> data = [];
-        Dictionary<string, bool> onlyUndocumentedCheckStates = [];
-        foreach (string kind in kinds)
-        {
-            VectorEntity[] allOfKind = sourceCode.Where(x => x.Kind == kind).OrderBy(x => x.Name).ToArray();
-            VectorEntity[] undocumentedOfKind = allOfKind.Where(x => string.IsNullOrWhiteSpace(x.Summary)).OrderBy(x => x.Name).ToArray();
-            VectorEntity[] documentedOfKind = allOfKind.Except(undocumentedOfKind).ToArray();
-            data.Add(new Data(kind, allOfKind, documentedOfKind, undocumentedOfKind));
-            onlyUndocumentedCheckStates.Add(kind, true);
-        }
-
-        _data = data;
-        _onlyUndocumentedCheckStates = onlyUndocumentedCheckStates;
+        _cSharpSources = Project.Sources.Where(x => x.Kind == ProjectSourceKind.CSharpCode).ToArray();
+        _selectedSource = _cSharpSources.FirstOrDefault();
+        await Refresh();
     }
 
-    private record Data(string Kind, VectorEntity[] All, VectorEntity[] Documented, VectorEntity[] UnDocumented)
+    private TreeItemData<Item> BuildTree(string rootPath, VectorEntity[] existing)
     {
-        public int Order => 1; //todo - set order based on importance of kind
-        public string TabCaption => $"{KindPlural} ({(Documented.Length)}/{All.Length})";
-        public string KindPlural => Kind + "s";
+        var rootInfo = new DirectoryInfo(rootPath);
+        var rootNode = new TreeItemData<Item> { Value = new Item(rootInfo, [], []) };
+        BuildChildren(rootNode, rootInfo, existing);
+        return rootNode;
     }
 
-    private void SwitchAllUndocumentedState(string dataEntryKind, bool newState)
+    private void BuildChildren(TreeItemData<Item> parentNode, DirectoryInfo dirInfo, VectorEntity[] existing)
     {
-        if (_onlyUndocumentedCheckStates != null)
+        parentNode.Children ??= [];
+        foreach (var dir in dirInfo.GetDirectories())
         {
-            _onlyUndocumentedCheckStates[dataEntryKind] = newState;
-            StateHasChanged();
+            var dirNode = new TreeItemData<Item> { Value = new Item(dir, [], []) };
+            BuildChildren(dirNode, dir, existing);
+            if (dirNode.Children is { Count: > 0 })
+            {
+                parentNode.Children.Add(dirNode);
+            }
+        }
+
+        foreach (var file in dirInfo.GetFiles("*.cs"))
+        {
+            List<CSharpChunk> entities = cSharpChunker.GetCodeEntities(File.ReadAllText(file.FullName)).Where(x => x.Kind == _kind).ToList();
+            var matches = entities.Where(x => x.Kind == _kind);
+            switch (_summaryStatus)
+            {
+                case SummaryStatus.MissingSummary:
+                    matches = matches.Where(x => string.IsNullOrWhiteSpace(x.XmlSummary));
+                    break;
+                case SummaryStatus.HasSummary:
+                    matches = matches.Where(x => !string.IsNullOrWhiteSpace(x.XmlSummary));
+                    break;
+            }
+
+            entities = matches.ToList();
+
+            var vectorMatches = existing.Where(x => file.FullName == _selectedSource.Path + x.SourcePath).ToArray();
+            if (entities.Count > 0)
+            {
+                parentNode.Children.Add(new TreeItemData<Item> { Value = new Item(file, entities, vectorMatches) });
+            }
         }
     }
 
-    private void SwitchSelectedItem(VectorEntity? entity)
+    private class Item(FileSystemInfo info, List<CSharpChunk> codeChunks, VectorEntity[] vectorEntities)
     {
-        if (entity == null)
+        public FileSystemInfo Info { get; } = info;
+        public List<CSharpChunk> CodeChunks { get; } = codeChunks;
+        public VectorEntity[] VectorEntities { get; } = vectorEntities;
+
+        public string GetText(List<TreeItemData<Item>>? children)
         {
-            return;
+            return Info.Name;
         }
 
-        if (_selectEntry?.VectorId != entity.VectorId)
+        public int GetTotalRelatedEntities(List<TreeItemData<Item>>? children)
         {
-            _selectEntry = entity;
-            _xmlSummary = null;
+            if (children == null) return CodeChunks.Count;
+            int sum = 0;
+            foreach (var child in children)
+            {
+                sum += child.Value.CodeChunks.Count;
+                sum += GetTotalRelatedEntities(child.Children);
+            }
+
+            return sum;
+        }
+
+        public string? GetIcon()
+        {
+            if (Info.Attributes.HasFlag(FileAttributes.Directory))
+            {
+                return Icons.Material.Filled.Folder;
+            }
+
+            return null;
         }
     }
 
-
-    private async Task GenerateXmlSummary()
+    private enum SummaryStatus
     {
-        _xmlSummary = await aiQuery.GenerateCSharpXmlSummary(Project, _selectEntry.Content);
+        All,
+        MissingSummary,
+        HasSummary
+    }
+
+    private async Task SwitchKind(CSharpKind kind)
+    {
+        _kind = kind;
+        await Refresh();
+    }
+
+    private async Task Refresh()
+    {
+        if (_selectedSource != null)
+        {
+            _existingVectorEntities = await vectorStoreQuery.GetExistingAsync(Project.Id, _selectedSource.Id);
+            var existing = _existingVectorEntities.Where(x => x.Kind == _kind.ToString());
+            switch (_summaryStatus)
+            {
+                case SummaryStatus.MissingSummary:
+                    existing = existing.Where(x => string.IsNullOrWhiteSpace(x.Summary));
+                    break;
+                case SummaryStatus.HasSummary:
+                    existing = existing.Where(x => !string.IsNullOrWhiteSpace(x.Summary));
+                    break;
+            }
+
+            _tree = BuildTree(_selectedSource.Path, existing.ToArray());
+        }
+    }
+
+    private async Task SwitchSummaryKind(SummaryStatus summaryStatus)
+    {
+        _summaryStatus = summaryStatus;
+        await Refresh();
+    }
+
+    private async Task SwitchSource(ProjectSourceEntity source)
+    {
+        _selectedSource = source;
+        if (_selectedSource != null)
+        {
+            await Refresh();
+        }
+        else
+        {
+            _existingVectorEntities = null;
+        }
+    }
+
+    private void SwitchSelected(Item? selectedItem)
+    {
+        _selectedItem = selectedItem;
+    }
+
+    private async Task Generate(CSharpChunk chunk)
+    {
+        var xmlSummary = await aiQuery.GenerateCSharpXmlSummary(Project, chunk.Content);
+        chunk.XmlSummary = xmlSummary;
+    }
+
+    private async Task Accept(string path, CSharpChunk chunk)
+    {
+        SyntaxTree tree = CSharpSyntaxTree.ParseText(await File.ReadAllTextAsync(path));
+        var root = await tree.GetRootAsync();
+
+        var oldNode = root.DescendantNodes().FirstOrDefault(x => x.ToString() == chunk.Node.ToString());
+
+        var xmlSummaryText = chunk.XmlSummary;
+        var parsedTrivia = SyntaxFactory.ParseLeadingTrivia(xmlSummaryText + Environment.NewLine);
+        var newMethod = oldNode.WithLeadingTrivia(parsedTrivia);
+
+        var newRoot = root.ReplaceNode(oldNode, newMethod);
+        await File.WriteAllTextAsync(path, newRoot.ToFullString().Replace("\r\n", "\n").Replace("\n", "\r\n"));
+    }
+
+
+    private async Task GenerateAll()
+    {
+        var workingProgress = BlazorUtils.StartWorking(_progressBar, _selectedItem.CodeChunks.Count, hideOnCompletion: true);
+        foreach (var chunk in _selectedItem.CodeChunks)
+        {
+            await Generate(chunk);
+            await Accept(_selectedItem.Info.FullName, chunk);
+            workingProgress.ReportProgress();
+        }
+
+        BlazorUtils.ShowSuccess("Done. Please check your GIT Changes for the repo");
     }
 }
