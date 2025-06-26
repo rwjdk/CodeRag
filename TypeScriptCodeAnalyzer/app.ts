8c61c83698a76aabee3ea39291aeaa5b2d944b67
Simon Walenkamp Hansen <swh@relewise.com>
2025-06-26 14:48:43 +0200

    class constructors

--- TypeScriptCodeAnalyzer/app.ts
+++ TypeScriptCodeAnalyzer/app.ts
@@ -1,25 +1,39 @@
 import { Project } from "ts-morph";
 
 const project = new Project({ tsConfigFilePath: "C:\\Users\\Simon\\Git\\relewise-sdk-javascript\\packages\\client\\tsconfig.json" });
-  const result = [];
+const result = [];
 
-  for (const sourceFile of project.getSourceFiles()) {
-    for (const cls of sourceFile.getClasses()) {
-      result.push({
-        file: sourceFile.getFilePath(),
-        className: cls.getName(),
-        methods: cls.getMethods().map(method => ({
-          name: method.getName(),
-          parameters: method.getParameters().map(p => ({
+for (const sourceFile of project.getSourceFiles()) {
+  for (const cls of sourceFile.getClasses()) {
+    // Get constructor info (if any)
+    const constructors = cls.getConstructors();
+    const constructorInfo = constructors.length > 0
+      ? constructors.map(ctor => ({
+          parameters: ctor.getParameters().map(p => ({
             name: p.getName(),
             type: p.getType().getText()
           })),
-          returnType: method.getReturnType().getText(),
-          docs: method.getJsDocs().map(doc => doc.getComment()).filter(Boolean)
-        })),
-        docs: cls.getJsDocs().map(doc => doc.getComment()).filter(Boolean)
-      });
-    }
-  }
+          docs: ctor.getJsDocs().map(doc => doc.getComment()).filter(Boolean)
+        }))
+      : [];
 
-  console.log(JSON.stringify(result));
+    result.push({
+      file: sourceFile.getFilePath(),
+      className: cls.getName(),
+      abstract: cls.isAbstract(), // <-- Add this line
+      constructor: constructorInfo,
+      methods: cls.getMethods().map(method => ({
+        name: method.getName(),
+        parameters: method.getParameters().map(p => ({
+          name: p.getName(),
+          type: p.getType().getText()
+        })),
+        returnType: method.getReturnType().getText(),
+        docs: method.getJsDocs().map(doc => doc.getComment()).filter(Boolean)
+      })),
+      docs: cls.getJsDocs().map(doc => doc.getComment()).filter(Boolean)
+    });
+  }
+}
+
+console.log(JSON.stringify(result));
